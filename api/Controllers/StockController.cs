113c875587ce4e6e0078d4296f042c54808ea4fa
Nair,Ajith <Ajith.Nair@Cerner.com>
2026-04-29 16:17:26 +0530

    PUT & DELETE

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -47,5 +47,41 @@ namespace api.Controllers
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new {id = stockModel.Id}, stockModel.ToStockDto());
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x=>x.Id == id);
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.MarketCap=updateDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x=>x.Id == id);
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
--- api/DTOs/Stock/CreateStockRequestDto.cs
+++ api/DTOs/Stock/CreateStockRequestDto.cs
@@ -14,4 +14,14 @@ namespace DTOs.Stock
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
     }
+
+    public class UpdateStockRequestDto
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public decimal Purchase { get; set; }
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+    }
 }
