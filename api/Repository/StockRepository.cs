104fc54e66e56160a2f0902f1a8dcfd22d5f0e32
Nair,Ajith <Ajith.Nair@Cerner.com>
2026-04-30 20:20:13 +0530

    Comment GetById + include

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -27,8 +27,8 @@ namespace api.Repository
 
         public async Task<Stock?> DeleteAsync(int id)
         {
-            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x=> x.Id == id);
-            if(stockModel==null)
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
             {
                 return null;
             }
@@ -39,22 +39,22 @@ namespace api.Repository
 
         public async Task<List<Stock>> GetAllAsync()
         {
-            return await _context.Stocks.ToListAsync();
+            return await _context.Stocks.Include(c => c.Comments).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
         {
-            return await _context.Stocks.FindAsync(id);
+            return await _context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(i => i.Id == id);
         }
 
         public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateDto)
         {
-            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x=> x.Id == id);
-            if(stockModel==null)
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
             {
                 return null;
             }
-            
+
             stockModel.Symbol = updateDto.Symbol;
             stockModel.CompanyName = updateDto.CompanyName;
             stockModel.Industry = updateDto.Industry;
