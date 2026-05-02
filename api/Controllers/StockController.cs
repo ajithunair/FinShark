0d4952065f12c1a2d8a2cd2d357502523e163380
Nair,Ajith <Ajith.Nair@Cerner.com>
2026-05-02 13:33:00 +0530

    Data Validations

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -28,14 +28,20 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var stocks = await _stockRepository.GetAllAsync();
             var stockDto = stocks.Select(s => s.ToStockDto());
             return Ok(stockDto);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var stock = await _stockRepository.GetByIdAsync(id);
 
             if (stock == null)
@@ -48,15 +54,21 @@ namespace api.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var stockModel = stockDto.ToStockFromCreateDTO();
             await _stockRepository.CreateAsync(stockModel);
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
 
         [HttpPut]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var stockModel = await _stockRepository.UpdateAsync(id, updateDto);
             if (stockModel == null)
             {
@@ -70,6 +82,9 @@ namespace api.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+                
             var stockModel = await _stockRepository.DeleteAsync(id);
             if (stockModel == null)
             {
--- api/DTOs/Stock/CreateStockRequestDto.cs
+++ api/DTOs/Stock/CreateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,21 +8,55 @@ namespace DTOs.Stock
 {
     public class CreateStockRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Symbol can not be less than 5 characters")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "CompanyName can not be less than 5 characters")]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 1000000000)]
         public decimal Purchase { get; set; }
+
+        [Required]
+        [Range(0.001, 100)]
         public decimal LastDiv { get; set; }
+
+        [Required]
+        [MinLength(10, ErrorMessage = "CompanyName can not be less than 10 characters")]
         public string Industry { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 5000000000)]
         public long MarketCap { get; set; }
     }
 
     public class UpdateStockRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Symbol can not be less than 5 characters")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "CompanyName can not be less than 5 characters")]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 1000000000)]
         public decimal Purchase { get; set; }
+
+        [Required]
+        [Range(0.001, 100)]
         public decimal LastDiv { get; set; }
+        
+        [Required]
+        [MinLength(10, ErrorMessage = "CompanyName can not be less than 10 characters")]
+
         public string Industry { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 5000000000)]
         public long MarketCap { get; set; }
     }
 }
